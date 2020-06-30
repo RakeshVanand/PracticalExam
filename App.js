2a7b3aac7c98fa8e67778208d94ed8ac5602bbe6
RakeshVanand <67641934+RakeshVanand@users.noreply.github.com>
2020-06-30 19:14:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { RootNavigator } from './src/RootNavigator';
+
+export default class App extends React.Component {
+  render() {
+    return (
+
+            < RootNavigator/>
+      
+    );
+  }
+}
